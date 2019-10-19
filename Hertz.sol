bd42468e6b673169be261c34ab15720aa80560db
Damir Olejar <olejar.damir@gmail.com>
2019-10-19 18:05:19 -0400

    Update Hertz.sol

--- Hertz.sol
+++ Hertz.sol
@@ -32,17 +32,20 @@ HZHZHZHZHZHZHZNm/ /mHZHZHZHZHZHZHZN
 |_|  |_________|  \_\ |_|  /_____| V1.0
      
 A stable-coin, with a constantly increasing price.
+
+ Symbol        :  HZ
+ Name          :  Hertz Token 
+ Total supply  :  21,000.0 (or 21 thousand tokens)
+ Decimals      :  18
+ Transfer Fees :  2% deducted from a transfer (a burning fee).
+ Exchange Fees :  2% of tokens deducted while purchasing, 
+                  no fees while taking back Ethereum
+ Author        :  Damir Olejar
+
+ 
 */
-// Symbol        :  HZ
-// Name          :  Hertz Token 
-// Total supply  :  21,000.0 (or 21 thousand tokens)
-// Decimals      :  18
-// Total Supply  :  Infinite, limit depends on how much people want to invest
-// Transfer Fees :  2% deducted from a transfer (a burning fee).
-// Exchange Fees :  2% of tokens deducted per exchange.
-// Author        :  Damir Olejar
-//
-// -----------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 // ----------------------------------------------------------------------------
 // Safe maths
