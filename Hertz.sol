e56655752955ee9d879e6679e8e96e371e91cbee
Damir Olejar <olejar.damir@gmail.com>
2019-10-19 21:16:08 -0400

    Update Hertz.sol

--- Hertz.sol
+++ Hertz.sol
@@ -237,10 +237,6 @@ contract _HERTZ is ERC20Interface, Owned {
 // ------------------------------------------------------------------------
 // Token owner can approve for `spender` to transferFrom(...) `tokens`
 // from the token owner's account
-//
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-// recommends that there are no checks for the approval double-spend attack
-// as this should be implemented in user interfaces
 // ------------------------------------------------------------------------
     function approve(address spender, uint tokens) public returns(bool success) {
         allowed[msg.sender][spender] = tokens;
